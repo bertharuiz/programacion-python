4392dc9cac58eaef4b2b6cfcbf49daadfc8943cd
bertharuiz <ber2613r@gmail.com>
2016-12-18 13:10:34 -0600

    Add files via upload

---
+++ Laboratorio 2/Ejercicio1.py
@@ -0,0 +1,8 @@
+#laboratorio 2
+#Bertha Domingo Ruiz
+
+import math
+
+x = float(input("lado 1:"))
+y = float(input("lado 2:"))
+print "Hipotenusa igual a: " +str(math.sqrt(x*x)+(y*y))
---
+++ Laboratorio 2/Ejercicio2.py
@@ -0,0 +1,8 @@
+#laboratorio 2
+#Bertha Domingo Ruiz
+
+banderazo=8.74
+def distancia(km):
+    n=(km/0.25)*1.84
+    dinero=n+banderazo
+    print dinero, "pesos"
